ded7d42792daf9798bcdf257285e1153abe72495
ryzke <dazryanjake7@gmail.com>
2025-03-16 00:59:18 +0800

    update welcome.php

--- welcome.php
+++ welcome.php
@@ -55,32 +55,32 @@ session_start();
   </style>
 </head>
 <body style=" width: 100%; height: 729px; display: flex; flex-direction: column; align-items: center; justify-content: flex-start; background-color: #e6e6ff; position: relative;">
-  <div style=" width: 60%; height: 30px; display: flex; justify-content: flex-end; align-items: center;  margin-top: 10px;">
-    <a href="login.php" style="border: 1px solid transparent; background-color: #0f57fe; padding: 8px 20px; border-radius: 50px; color: white; text-align: center; cursor: pointer;">log out</a>
+  <div style=" width: 60%; height: 30px; display: flex; justify-content: flex-end; align-items: center;  margin-top: 60px;">
+    <a href="login.php" style="border: 1px solid transparent; background-color: #0f57fe; padding: 8px 20px; border-radius: 50px; color: white; text-align: center; cursor: pointer; text-decoration: none;">Log out</a>
   </div>
   <h1 style=" width: 60% height: 50px; text-align: center; font-size: 30pt; margin-bottom: 20px;">  
     List of Users
   </h1>
-  <table style=" width: 900px; border: 2px solid black; text-align: center; background-color: white;">
-        <tr  style="border: 1px solid black; background-color: #b3c6ff;">
-            <th style="border: 2px solid black;">ID</th>
-            <th style="border: 2px solid black;">First Name</th>
-            <th style="border: 2px solid black;">Last Name</th>
-            <th style="border: 2px solid black;">Contact Number</th>
-            <th style="border: 2px solid black;">Username</th>
-            <th style="border: 2px solid black;">Password</th>
-            <th style=" width: 30px; border: 2px solid black;">Action</th>
+  <table style=" width: 900px; border-collapse: collapse; border: 1px solid black; text-align: center; background-color: white;">
+        <tr style="border: 1px solid black;; background-color: #b3c6ff;">
+            <th style="border: 1px solid black;">ID</th>
+            <th style="border: 1px solid black;">First Name</th>
+            <th style="border: 1px solid black;">Last Name</th>
+            <th style="border: 1px solid black;">Contact Number</th>
+            <th style="border: 1px solid black;">Username</th>
+            <th style="border: 1px solid black;">Password</th>
+            <th style=" width: 30px; border: 1px solid black;">Action</th>
         </tr>
             <?php
                 if($showlist->num_rows > 0){
                   while($row = $showlist->fetch_assoc()){
                     echo "<tr class='list' style='border: 1px solid black; height: 50px;'>
-                          <td>{$row['id']}</td>
-                          <td style= text-align: left;  padding-left: 16px; border: 1px solid black;>{$row['first_name']}</td>
-                          <td style=' text-align: left; padding-left: 16px; border: 1px solid black;'>{$row['last_name']}</td>
-                          <td style=' border: 1px solid black;'>{$row['contact_number']}</td>
-                          <td style=' border: 1px solid black;'>{$row['username']}</td>
-                          <td style=' border: 1px solid black;'>{$row['password']}</td>
+                          <td style='text-align: left;  padding-left: 16px;'>{$row['id']}</td>
+                          <td style= 'text-align: left;  padding-left: 16px;'>{$row['first_name']}</td>
+                          <td style=' text-align: left; padding-left: 16px;'>{$row['last_name']}</td>
+                          <td style=' '>{$row['contact_number']}</td>
+                          <td style=' '>{$row['username']}</td>
+                          <td style=' '>{$row['password']}</td>
                           <td style=' display: flex; flex-direction: row; align-items: center; height: 50px;'>    
                               <form method='POST' style='height: 100%; display: flex; align-items: center;'>
                                   <input type='hidden' name='emp_id' value='{$row['id']}'>
